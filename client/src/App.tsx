48f7be48d6740862bb351aca8394b564b59dd10f
christianjward <christian.ward@gmail.com>
2025-04-25 23:28:09 +0100

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import HomePage from "@/pages/home-page";
-import TracksPage from "@/pages/tracks-page";
-import AdminPage from "@/pages/admin-page";
-import AboutPage from "@/pages/about-page";
-import ContactPage from "@/pages/contact-page";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-import GlobalPlayer from "@/components/global-player";
-import { AudioPlayerProvider } from "@/hooks/use-audio-player";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import NotFound from "./pages/not-found";
+import HomePage from "./pages/home-page";
+import TracksPage from "./pages/tracks-page";
+import AdminPage from "./pages/admin-page";
+import AboutPage from "./pages/about-page";
+import ContactPage from "./pages/contact-page";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import GlobalPlayer from "./components/global-player";
+import { AudioPlayerProvider } from "./hooks/use-audio-player";
 
 function Router() {
   return (
